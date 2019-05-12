6aa504e9fc7d45f72d5d584493dcd9cfb982df3a
wetsmith <westhomsmit@gmail.com>
2019-05-12 15:42:58 -0700

    hw2p1
    p2 pending

---
+++ Homework 2/Part 1/Homework 2 Part 1/Assets/shiphover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shiphover : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float accelx = 0.2f;
+        float accely = 0.02f;
+        float accelz = .5f;
+        transform.Rotate(accelx * Mathf.Cos(Time.time), 0, accelz * Mathf.Cos(Time.time));
+        transform.Translate(accelx * 0.3f * Mathf.Cos(Time.time), accely * Mathf.Cos(Time.time * 5),0);
+    }
+}
