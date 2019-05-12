8d36bc1137100030a3d8286cab9da17d7ef94ead
wetsmith <westhomsmit@gmail.com>
2019-05-12 15:46:13 -0700

    hw2 p2
    commit

---
+++ Homework 2/Part 2/HW2P2/Assets/camshift.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camshift : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float accelx = 0.02f;
+        float accely = 0.2f;
+        float accelz = 0.2f;
+
+        transform.Translate(accelx * Mathf.Cos(Time.time),0, 0);
+    }
+}
---
+++ Homework 2/Part 2/README.txt
@@ -0,0 +1,3 @@
+This scene shows a reflective, icy water across a glacial noise-generated terrain. Continuing my effort to learn how to use ShaderGraph, I created the terrain system in shader graph.
+
+The executable build can be found in the /HW2P2/Build folder.
